0218620f2a20683b63fa534387d81a7b40f25b1f
Korsaurus <grkorsos@gmail.com>
2021-11-05 13:10:29 -0600

    L

---
+++ src/components/intro/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css"
+
+export default function Intro() {
+
+    return (
+        <div className="header">
+            <h1>Greg Korsos</h1>
+            <p><em>Front-End Developer</em></p>
+            <div className="icons">github linkedin twitter</div>
+            <p>I am a self taught developer with skill in React, HTML, CSS, Javascript, and experience with socket.io, Express, nodejs. My goal over the next  year is to become a Full-Stack Developer. I live for the satisfaction of solving problems and learning as much as I can about complex subjects.</p>
+            <br />
+            <a>My CV</a>
+        </div>
+    )
+}
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,10 +1,99 @@
 import React from "react";
+import "./projects.css"
+import Gregori from "../../utils/imgs/Gregori.png"
 
-export function Projects() {
+export default function Projects() {
 
     return (
         <div>
             <h2>Projects</h2>
+                <article>
+                    <div className="project-imgwrap">
+                        <img className="project-image" src={Gregori} alt="" />
+                    </div>
+                    <div className="project-textwrap">
+                        <h3>GREGORIS PIZZA</h3>
+                        <div >
+                        <p className="project-text">
+                            GREGORIS PIZZA BABIDABOOPY
+                        </p>
+                        </div>
+                        <a href="https://gregorispizza.netlify.app/">Link to live demo.</a>
+                    </div>
+                </article>
+                <article>
+                    <div className="project-imgwrap-right">
+                        <img className="project-image-right" src={Gregori} alt="" />
+                    </div>
+                <div className="project-textwrap-left">
+                        <h3></h3>
+                        <div >
+                        <p className="project-text-left">
+                            2
+                        </p>
+                        </div>
+                        <a href="">Link to live demo.</a>
+                    </div>
+                </article>
+                <article>
+                    <div className="project-imgwrap">
+                        <img className="project-image" src={Gregori} alt="" />
+                    </div>
+                    <div className="project-textwrap">
+                        <h3>GREGORIS PIZZA</h3>
+                        <div >
+                        <p className="project-text">
+                            GREGORIS PIZZA BABIDABOOPY
+                        </p>
+                        </div>
+                        <a href="https://gregorispizza.netlify.app/">Link to live demo.</a>
+                    </div>
+                </article>
+                <br />
+                <article>
+                    <div className="project-imgwrap-right">
+                        <img className="project-image-right" src={Gregori} alt="" />
+                    </div>
+                <div className="project-textwrap-left">
+                        <h3>GREGORIS PIZZA</h3>
+                        <div >
+                        <p className="project-text-left">
+                            GREGORIS PIZZA BABIDABOOPY
+                        </p>
+                        </div>
+                        <a href="https://gregorispizza.netlify.app/">Link to live demo.</a>
+                    </div>
+                    
+                </article>
+                <article>
+                    <div className="project-imgwrap">
+                        <img className="project-image" src={Gregori} alt="" />
+                    </div>
+                    <div className="project-textwrap">
+                        <h3>GREGORIS PIZZA</h3>
+                        <div >
+                        <p className="project-text">
+                            GREGORIS PIZZA BABIDABOOPY
+                        </p>
+                        </div>
+                        <a href="https://gregorispizza.netlify.app/">Link to live demo.</a>
+                    </div>
+                </article>
+                <br />
+                <article>
+                    <div className="project-imgwrap-right">
+                        <img className="project-image-right" src={Gregori} alt="" />
+                    </div>
+                <div className="project-textwrap-left">
+                        <h3>GREGORIS PIZZA</h3>
+                        <div >
+                        <p className="project-text-left">
+                            GREGORIS PIZZA BABIDABOOPY
+                        </p>
+                        </div>
+                        <a href="https://gregorispizza.netlify.app/">Link to live demo.</a>
+                    </div>                    
+                </article>
         </div>
     )
 }
